c668165ff6e3957eab836a67b7e3446b94583d3d
jerrylai19990120 <jerrylai19990120@gmail.com>
2023-10-30 00:53:46 -0400

    ready to deploy

--- index.js
+++ index.js
@@ -44,7 +44,23 @@
 // })
 // exports.app = functions.https.onRequest(app);
 const express = require("express")
+var cors = require('cors')
 const app = express()
+const express = require('express')
+var cors = require('cors')
+const http = require('http');
+var request = require('request');
+const bodyParser = require('body-parser');
+var functions = require('firebase-functions')
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json())
+
+var corsOptions = {
+    origin: ['http://localhost:4200', 'https://miniauglarsite.web.app'],
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+var openAiLink = "https://api.openai.com/v1/chat/completions"
+app.use(cors())
 
 // use the express-static middleware
 app.use(express.static("public"))
@@ -56,6 +72,25 @@ app.get("/", function (req, res) {
 
 app.get('/test', (req, res) => {
     res.send("up and running")
+})
+
+app.post('/getOpenAI', (req, res) => {
+    var clientServerOptions = {
+        url: openAiLink,
+        body: JSON.stringify(req.body),
+        method: 'POST',
+        headers: {
+            "Authorization": `Bearer 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`,
+            "Content-Type": 'application/json',
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "POST, GET, PUT",
+            "Access-Control-Allow-Headers": "Content-Type",
+                }
+    }
+    
+    request(clientServerOptions, function (error, response) {
+        res.send(response.body)
+    });
 })
 
 
